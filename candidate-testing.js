0d29182bdaf378d0bcc509829ea9f58f76478010
M T <85591927+mteszner@users.noreply.github.com>
2021-06-22 23:17:35 +0000

    Working on Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,42 +3,51 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = [question, "True or false: 5 kilometer == 5000 meters? ", "(5 + 3) / 2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS?"];
+let correctAnswers = [correctAnswer, true, 40, "Trajectory", 3];
+let candidateAnswers = [];
+//you will need an array of the user's answers and something to compare correctAnswers and candidateAnswers
 
+// TESTING - arrays display OK
+console.log("")
+// TESTING
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("What is your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+candidateAnswer = input.question(question)
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
+if (candidateAnswer === correctAnswer) {
+    console.log("You're right!")
+  } else {
+    console.log("Try again!")
+  }
+// I didn't use the let/return provided below. Is this ok?
   let grade;
-  
+
 
   return grade;
 }
-
+//^^^
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hello, " + candidateName)
+  // TODO ^added that but blank answer in console? - FIXED  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
